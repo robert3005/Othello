326cce3e3776927ccb58ba220e0565560d315e99
Robert Kruszewski <dzobert@gmail.com>
2011-11-01 17:18:12 +0000

    changed the object creation method, anyway we won\'t add methods to oject during execution

--- Reversi.js
+++ Reversi.js
@@ -1,48 +1,46 @@
 //---------------------------------------------------------
 // Grid's functions
-function Grid(width, height){
-	this.width = width;
-	this.height = height;
-	this.cells = new Array(width*height);
-	this.populate();
-}
+var Grid = function(size){
+	this.size = size;
+	this.cells = [];
 
-Grid.prototype.populate = function(){
-	for(var y = 0; y < this.height; ++y){
-		for(var x = 0; x < this.width; ++x){
-			this.setFieldAt(x, y, new Field());
+	populate: function(){
+		for(var y = 0; y < this.height; ++y){
+			for(var x = 0; x < this.width; ++x){
+				this.setFieldAt(x, y, new Field());
+			}
 		}
-	}
-}
+	},	
 
-Grid.prototype.getFieldAt = function(x, y){
-	return this.cells[y * this.width + x];	
-};
+	getFieldAt: function(x, y){
+		return this.cells[y * this.width + x];	
+	},
+	
+	setFieldAt: function(x, y, field){
+		this.cells[y * this.width + x] = field;
+	},
 
-Grid.prototype.setFieldAt = function(x, y, field){
-	this.cells[y * this.width + x] = field;
-};
-
-Grid.prototype.each = function(action){
-	for(var y = 0; y < this.height; ++y){
-		for(var x = 0; this.width; ++x){
-			action(x, y, this.getFieldAt(x, y));
+	each: function(action){
+		for(var y = 0; y < this.height; ++y){
+			for(var x = 0; this.width; ++x){
+				action(x, y, this.getFieldAt(x, y));
+			}
 		}
-	}
-};
+	};
+}
 //---------------------------------------------------------
 // Field's functions
-function Field(color){
-	this.setColor(color);
+var Field = function(color){
+	this.color;
+
+	setColor: function(color){
+		this.color = color;
+	},
+
+	getColor: function(){
+		return this.color;		
+	};
 }
-
-Field.prototype.setColor = function(color){
-	this.color = color;
-};
-
-Field.prototype.getColor = function(){
-	return this.color;		
-};
 
 var colorsEnum = {black:1, white:2};
 Object.freeze(colorsEnum);
@@ -50,66 +48,61 @@ Object.freeze(colorsEnum);
 //---------------------------------------------------------
 // Player's functions
 
-function Player(id, players, character){
+var Player = function(id, color){
 	this.id = id;
-	this.character = character;
+	this.color = color;
 	this.score = 0;
-	players.push(this);
+	
+	makeMove: function(grid){
+		var move = "";//prompt("What's your next move, busy lady?(type in the coordinates in format 'width height' ", "");
+		var coords = convertInput(move);
+		checkIfLegal(coords, grid);
+		updateGrid(coords, grid);
+	};
 }
-
-Player.prototype.makeMove = function(grid){
-	var move = "";//prompt("What's your next move, busy lady?(type in the coordinates in format 'width height' ", "");
-	var coords = convertInput(move);
-	checkIfLegal(coords, grid);
-	updateGrid(coords, grid);
-};
 
 //---------------------------------------------------------
 // Game's functions
 
 function Game(size){
-	this.players = new Array(2);
-	var player1 = new Player(1, this.players, 'x');
-	var player2 = new Player(2, this.players, 'o');
-	this.grid = this.initialise(size);
-}
+	this.players = [];
+	this.grid = new Grid(size);
+	
+	initialise: function(size){
+		grid.getFieldAt(grid.width/2, grid.height/2).setColor(colorsEnum.white);
+		grid.getFieldAt(grid.width/2+1, grid.height/2+1).setColor(colorsEnum.white);
+		grid.getFieldAt(grid.width/2+1, grid.height/2).setColor(colorsEnum.black);
+		grid.getFieldAt(grid.width/2, grid.height/2+1).setColor(colorsEnum.black);
+	},
 
-Game.prototype.initialise = function(size){
-	var grid = new Grid(size, size);
-	grid.getFieldAt(grid.width/2, grid.height/2).setColor(colorsEnum.white);
-	grid.getFieldAt(grid.width/2+1, grid.height/2+1).setColor(colorsEnum.white);
-	grid.getFieldAt(grid.width/2+1, grid.height/2).setColor(colorsEnum.black);
-	grid.getFieldAt(grid.width/2, grid.height/2+1).setColor(colorsEnum.black);
-	return grid;
-};
-
-Game.prototype.play = function(){
-	var i = 0;
-	while(!isGameFinished()){
-		this.players[i].makeMove();
-		//alert(this.toString);
-		//alert(this.printScore);
-		i = (i+1)%2;
-	}
+	play: function(){
+		var i = 0;
+		while(!isGameFinished()){
+			this.players[i].makeMove();
+			//alert(this.toString);
+			//alert(this.printScore);
+			i = (i+1)%2;
+		}
 	//alert("Game is over./n")
 	//alert(this.printScore);
-};
+	},
+	
+	printScore: function(){
+		return "The score is: player 1 (" + players[0].score + 
+		") and player 2 (" + players[1].score + "/n";
+	},
 
-Game.prototype.printScore = function(){
-	return "The score is: player 1 (" + players[0].score + 
-	") and player 2 (" + players[1].score + "/n";
+	toString: function() {
+		var characters = [];
+		var endOfLine = this.grid.width - 1;
+		this.grid.each(function(x, y, value){
+			characters.push(characterFromElement(value));
+			if(x == endOfLine)
+				characters.push("\n");
+		});
+		return characters.join("");
+	};
 }
-
-Game.prototype.toString = function() {
-	var characters = [];
-	var endOfLine = this.grid.width - 1;
-	this.grid.each(function(x, y, value){
-		characters.push(characterFromElement(value));
-		if(x == endOfLine)
-			characters.push("\n");
-	});
-	return characters.join("");
-};
 
 //---------------------------------------------------------
 // Other functions (maybe prototype methods)
