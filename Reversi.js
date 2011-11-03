f9c71cdcc5f5da84da1c2f2a8c92403dd4e92a81
Piotr Bar <baru02@gmail.com>
2011-11-03 20:51:30 +0000

    beauty

--- Reversi.js
+++ Reversi.js
@@ -1,243 +1,238 @@
 // One Global // dunno about it // 
 var MY = {};
-
 ///////////////////////////////////////////////////////////
-// Grid's functions //
-var Grid = function(size){
-	this.size = size;
-	this.cells = [];
+// Dictionary //
+var Dictionary = function (startValues) {
+        "use strict";
+        this.values = startValues || {};
 
-		populate: function(){
-			for(var y = 0; y < this.height; ++y){
-				for(var x = 0; x < this.width; ++x){
-					this.setFieldAt(x, y, new Field());
-				}
-			}
-		},	
+        this.store = function (name, value) {
+            this.values[name] = value;
+        };
 
-		getFieldAt: function(x, y){
-			return this.cells[y * this.width + x];	
-		},
-		
-		setFieldAt: function(x, y, field){
-			this.cells[y * this.width + x] = field;
-		},
+        this.lookup = function (name) {
+            return this.values[name];
+        };
 
-		each: function(action){
-			for(var y = 0; y < this.height; ++y){
-				for(var x = 0; this.width; ++x){
-					action(x, y, this.getFieldAt(x, y));
-				}
-			}
-		},
+        this.contains = function (name) {
+            return Object.prototype.propertyIsEnumerable.call(this.values, name);
+        };
 
-		addTo: function(state, value){
-			return [state[0] + value[0], state[1] + value[1]];
-		};
-};
+        this.forEachIn = function (object, action) {
+            for (var property in object) {
+                if (Object.prototype.hasOwnProperty.call(object, property)) {
+                    action(property, object[property]);
+                }
+            }
+        };
+
+        this.each = function (action) {
+            this.forEachIn(this.values, action);
+        };
+
+
+    };
 
 ///////////////////////////////////////////////////////////
 // Field's functions //
-var Field = function(color){
-	this.color = colorsEnum.empty;
-
-	setColor: function(color){
-		this.color = color;
-	},
-
-	getColor: function(){
-		return this.color;		
-	};
-
+MY.colorsEnum = {
+    white: 1,
+    black: 2,
+    empty: 3
 };
-
-MY.colorsEnum = {black:1, white:2, empty:3};
 Object.freeze(MY.colorsEnum);
+
+var Field = function (color) {
+        "use strict";
+        this.color = MY.colorsEnum.empty;
+
+        this.setColor = function (color) {
+            this.color = color;
+        };
+
+        this.getColor = function () {
+            return this.color;
+        };
+
+    };
+
+///////////////////////////////////////////////////////////
+// Grid's functions //
+var Grid = function (size) {
+        "use strict";
+        this.size = size;
+        this.cells = [];
+
+        this.populate = function () {
+            for (var y = 0; y < this.height; ++y) {
+                for (var x = 0; x < this.width; ++x) {
+                    this.setFieldAt(x, y, new Field());
+                }
+            }
+        };
+
+        this.getFieldAt = function (x, y) {
+            return this.cells[y * this.width + x];
+        };
+
+        this.setFieldAt = function (x, y, field) {
+            this.cells[y * this.width + x] = field;
+        };
+
+        this.each = function (action) {
+            for (var y = 0; y < this.height; ++y) {
+                for (var x = 0; this.width; ++x) {
+                    action(x, y, this.getFieldAt(x, y));
+                }
+            }
+        };
+
+        this.addTo = function (state, value) {
+            return [state[0] + value[0], state[1] + value[1]];
+        };
+    };
 
 ///////////////////////////////////////////////////////////
 // Player's functions //
-
-var Player = function(id, color){
-	this.id = id;
-	this.color = color;
-	this.score = 0;
-	this.legalMoves = [];
-};
+var Player = function (id, color) {
+        "use strict";
+        this.id = id;
+        this.color = color;
+        this.score = 0;
+        this.legalMoves = [];
+    };
 
 ///////////////////////////////////////////////////////////
 // Game's functions //
+var Game = function (size) {
+        "use strict";
+        this.players = [];
+        this.grid = new Grid(size);
+        this.pass = false; // in the beginning no player could pass
+        this.currentPlayer = {};
 
-var Game = function(size){
-	this.players = [];
-	this.grid = new Grid(size);
-  	this.pass = false;  // in the beginning no player could pass
-  	this.currentPlayer = {};
-	
-	initialise: function(){
-		var middleField = grid.size/2;
-		this.grid.getFieldAt(middleField, middleField).setColor(MY.colorsEnum.white);
-		this.grid.getFieldAt(middleField+1, middleField+1).setColor(MY.colorsEnum.white);
-		this.grid.getFieldAt(middleField+1, middleField).setColor(MY.colorsEnum.black);
-		this.grid.getFieldAt(middleField, middleField+1).setColor(MY.colorsEnum.black);
-	},
+        this.initialise = function () {
+            var middleField = this.grid.size / 2;
+            this.grid.getFieldAt(middleField, middleField).setColor(MY.colorsEnum.white);
+            this.grid.getFieldAt(middleField + 1, middleField + 1).setColor(MY.colorsEnum.white);
+            this.grid.getFieldAt(middleField + 1, middleField).setColor(MY.colorsEnum.black);
+            this.grid.getFieldAt(middleField, middleField + 1).setColor(MY.colorsEnum.black);
+        };
 
-	play: function(){
-		var i = 0;
-		this.currentPlayer = players[i];
-		this.currentPlayer.legalMoves = getLegalMoves();
-		while(!isGameFinished()){
-			if(!this.pass){
-				this.makeMove();
-			}
-			i = (i+1)%2;
-			this.currentPlayer = players[i];
-			this.currentPlayer.legalMoves = getLegalMoves();
-		}
-	//alert("Game is over./n")
-	//alert(this.printScore);
-	},
+        this.play = function () {
+            var i = 0;
+            var changePlayer = function () {
+                    this.currentPlayer = this.players[i];
+                    this.currentPlayer.legalMoves = this.getLegalMoves();
+                };
 
-	isGameFinished: function(){
-    if(this.currentPlayer.legalMoves === [] && this.pass) return true; // if the currPlayer can't move and the prevPlayer passed then we finish the game
-    if(this.currentPlayer.legalMoves === []) this.pass = true;        // if the current player can't move then he must pass and we have to check whether the second player can move
-    else this.pass = false;                                           // if currPlayer can move we have to change the valueof pass to false
-    return false;  
-  	},
-	  //return (this.currentPlayer.legalMoves === []) ? !this.pass : true;
+            this.initialise();
+            this.players.push(new Player(1, MY.colorsEnum.white));
+            this.players.push(new Player(2, MY.colorsEnum.black));
+            changePlayer();
+            while (!this.isGameFinished()) {
+                if (!this.pass) {
+                    this.makeMove();
+                }
+                i = (i + 1) % 2;
+                changePlayer();
+            }
+            //alert("Game is over./n")
+            //alert(this.printScore);
+        };
 
-	makeMove: function(){
-		var coords = [0,0];  // read Coords here
-		updateGrid(coords);
-	},
+        this.isGameFinished = function () {
+            if (this.currentPlayer.legalMoves === [] && this.pass) {
+                return true;
+            } // if the currPlayer can't move and the prevPlayer passed then we finish the game
+            if (this.currentPlayer.legalMoves === []) {
+                this.pass = true;
+            } // if the current player can't move then he must pass and we have to check whether the second player can move
+            else {
+                this.pass = false;
+            } // if currPlayer can move we have to change the valueof pass to false
+            return false;
+        };
+        //return (this.currentPlayer.legalMoves === []) ? !this.pass : true;
+        this.makeMove = function () {
+            var coords = [0, 0]; // read Coords here
+            this.updateGrid(coords);
+        };
 
-	getLegalMoves: function(){
-		var legalMoves = new Dictionary,tilesToChange = [], tempTiles = [], localColor, state = [], outOutBounds = false;
-	
-		var advance = function(){
-			state = grid.addTo(state, value);
-			if(state[0] == this.grid.size || state[1] == this.grid.size){
-				outOfBounds = true;
-			} else {
-				localColor = grid.getFieldAt(state[0], state[1]).getColor;
-			}
-		};
+        this.getLegalMoves = function () {
+            var legalMoves = new Dictionary(),
+                tilesToChange = [],
+                tempTiles = [],
+                localColor, state = [],
+                outOfBounds = false;
 
-		grid.each(function(x, y){
-			state = [x, y];
-			if(this.grid.getFieldAt(x, y).getColor == MY.colorsEnum.empty){
-				this.directions.each(function(direction, value){
-					advance();
-					while(!outOfBounds && localColor != currentPlayer.getColor && localColor != MY.colorsEnum.empty){
-						tempTiles.push(state);
-						advance();
-					}
-					if(outOfBounds && localColor == MY.colorsEnum.empty){
-						tempTiles.clear;
-					}
-					tilesToChange = tilesToChange.concat(tempTiles);
-				});
-				if(tilesToChange.size > 0){
-					legalMoves.store([x, y], tilesToChange);
-				}
-				tilesToChange.clear;
-			}	
-		});
-		return legalMoves;
-	},
+            var advance = function (value) {
+                    state = this.grid.addTo(state, value);
+                    if (state[0] === this.grid.size || state[1] === this.grid.size) {
+                        outOfBounds = true;
+                    } else {
+                        localColor = this.grid.getFieldAt(state[0], state[1]).getColor;
+                    }
+                };
 
-	updateGrid: function(coords){
-		var tilesToChange = this.currentPlayer.legalMoves.lookup([coords[0], coords[1]]),
-			fieldToChange;
-		this.currentPlayer.score += tilesToChange.size;
-		for(var i = 0; i < tilesToChange.size; ++i){
-			fieldToChange = tilesToChange[i];
-			this.grid.getFieldAt(fieldToChange[0], fieldToChange[1]).setColor(this.currentPlayer.getColor);
-		}
-	},
+            this.grid.each(function (x, y) {
+                state = [x, y];
+                if (this.grid.getFieldAt(x, y).getColor === MY.colorsEnum.empty) {
+                    this.directions.each(function (direction, value) {
+                        advance(value);
+                        while (!outOfBounds && localColor !== this.currentPlayer.getColor && localColor !== MY.colorsEnum.empty) {
+                            tempTiles.push(state);
+                            advance(value);
+                        }
+                        if (outOfBounds && localColor === MY.colorsEnum.empty) {
+                            tempTiles.clear();
+                        }
+                        tilesToChange = tilesToChange.concat(tempTiles);
+                    });
+                    if (tilesToChange.size > 0) {
+                        legalMoves.store([x, y], tilesToChange);
+                    }
+                    tilesToChange.clear();
+                }
+            });
+            return legalMoves;
+        };
 
-	directions: function() {return new Dictionary(
-		{"n": [0, -1],
-		"ne": [1, -1],
-		"e":  [1, 0],
-		"se": [1, 1],
-		"s": [0, 1],
-		"sw": [-1, 1],
-		"w": [-1, 0],
-		"nw": [-1, -1]});
-	};
+        this.updateGrid = function (coords) {
+            var tilesToChange = this.currentPlayer.legalMoves.lookup([coords[0], coords[1]]),
+                fieldToChange;
+            this.currentPlayer.score += tilesToChange.size;
+            for (var i = 0; i < tilesToChange.size; ++i) {
+                fieldToChange = tilesToChange[i];
+                this.grid.getFieldAt(fieldToChange[0], fieldToChange[1]).setColor(this.currentPlayer.getColor);
+            }
+        };
 
-};
-
-///////////////////////////////////////////////////////////
-// Dictionary //
-
-var Dictionary = function(startValues) {
-	this.values = startValues || {};
-
-	store: function(name, value) {
-		this.values[name] = value;
-	},
-
-	lookup: function(name) {
-		return this.values[name];
-	},
-
-	contains: function(name) {
-		return Object.prototype.propertyIsEnumerable.call(this.values, name);
-	},
-	each: function(action) {
-		forEachIn(this.values, action);
-	},
-
-	forEachIn: function(object, action) {
-		for (var property in object) {
-			if (Object.prototype.hasOwnProperty.call(object, property))
-				action(property, object[property]);
-	}
-	};
-};
+        this.directions = new Dictionary({
+            "n": [0, -1],
+            "ne": [1, -1],
+            "e": [1, 0],
+            "se": [1, 1],
+            "s": [0, 1],
+            "sw": [-1, 1],
+            "w": [-1, 0],
+            "nw": [-1, -1]
+        });
+    };
 
 ///////////////////////////////////////////////////////////
 // main //
-
-var main = function(){
-	while(1){
-		var size=8;// = prompt("Could you enter the size of the grid (only even numbers): ", "");
-		if(size%2==0)
-			break;
-	}
-	var game = new Game(size);
-	game.play;
-};
+var main = function () {
+        "use strict";
+        var size, game;
+        while (1) {
+            size = 8; // = prompt("Could you enter the size of the grid (only even numbers): ", "");
+            if (size % 2 === 0) {
+                break;
+            }
+        }
+        game = new Game(size);
+        game.play();
+    };
 
 main();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
