0bceaf6d6260fca95221257ff9b7f8a47f45215f
Robert Kruszewski <dzobert@gmail.com>
2011-11-11 01:59:20 +0000

    Refactoring of UI code, added restart button

--- ReversiUI.js
+++ ReversiUI.js
@@ -1,8 +1,9 @@
 window.onload = function () {
 	var ReversiUI = function () {
 			"use strict";
-			this.boardColor = "#333";
-			this.cellBorderColor = "#422faa";
+			this.boardColorLight = "#B2CBD9";
+			this.boardColorDark = "#7F8E98";
+			this.boardBorderColor = "#EFDAAD";
 			this.whites = "#E5EEF5";
 			this.blacks = "#4F443A";
 			this.tokenBorder = "#000";
@@ -11,28 +12,25 @@ window.onload = function () {
 			this.baseOffset = 100;
 			this.gridsize = this.size * this.cellsize + 2 * this.baseOffset;
 			this.Game;
-			this.drawingObject;
+			this.drawable;
 			this.header;
-			this.scores = [];
-			this.scoreBoard = [
-				[],
-				[]
-			];
-			this.fieldList = [];
+			this.scores;
+			this.scoreBoard;
+			this.fieldList;
 
 			this.field = function (xcord, ycord, column, row) {
 				var that = this;
 				var colour = this.Game.grid.getFieldAt(row, column).color;
 				var defined = colour === MY.colorsEnum.empty ? false : true;
 				colour = colour === MY.colorsEnum.white ? this.whites : this.blacks;
-				var backgroundColour = ((row * this.size +  column) % 2) === row % 2 ? '#B2CBD9' : '#7F8E98';
+				var backgroundColour = ((row * this.size +  column) % 2) === (row % 2) ? this.boardColorLight  : this.boardColorDark;
 
-				var fieldBackground = this.drawingObject.rect(xcord, ycord, this.cellsize, this.cellsize).attr({
+				var fieldBackground = this.drawable.rect(xcord, ycord, this.cellsize, this.cellsize).attr({
 					fill: backgroundColour,
-					stroke: '#EFDAAD'
+					stroke: this.boardBorderColor
 				});
 
-				var token = this.drawingObject.circle(xcord + this.cellsize / 2, ycord + this.cellsize / 2, 20).attr({
+				var token = this.drawable.circle(xcord + this.cellsize / 2, ycord + this.cellsize / 2, 20).attr({
 					fill: colour,
 					stroke: this.tokenBorder,
 					opacity: defined ? 1 : 0
@@ -50,7 +48,7 @@ window.onload = function () {
 									}, 200, "backOut");
 								}
 							}
-						}
+						};
 					};
 
 				var placeToken = function (obj) {
@@ -59,12 +57,12 @@ window.onload = function () {
 								token.data("defined", true);
 								obj.makeMove(row, column);
 							}
-						}
+						};
 					};
 
-				var eventField = this.drawingObject.rect(xcord, ycord, this.cellsize, this.cellsize).attr({
-					fill: this.boardColor,
-					stroke: this.cellBorderColor,
+				var eventField = this.drawable.rect(xcord, ycord, this.cellsize, this.cellsize).attr({
+					fill: this.boardColorLight,
+					stroke: this.boardBorderColor,
 					opacity: 0
 				}).hover(toggleTokenColor(0.4), toggleTokenColor(0), this, this).click(placeToken(this));
 				return token.id;
@@ -73,32 +71,50 @@ window.onload = function () {
 			this.initialise = function () {
 				this.Game = new Game(this.size);
 				this.Game.initialise();
-			}
+							this.scores = [];
+			this.scoreBoard = [
+				[],
+				[]
+			];
+			this.fieldList = [];
+			};
 
 			this.makeMove = function (row, column) {
 				this.Game.makeMove(row, column);
+			
 				var tilesToChange = this.Game.currentPlayer.legalMoves.lookup([row, column]);
-				var tilesDrawables = this.drawingObject.set();
+				var tilesDrawables = this.drawable.set();
 				tilesToChange.forEach(function (coords, index, array) {
-					tilesDrawables.push(this.drawingObject.getById(this.fieldList[coords[1] * this.size + coords[0]]))
+					tilesDrawables.push(this.drawable.getById(this.fieldList[coords[1] * this.size + coords[0]]));
 				}, this);
-				tilesDrawables.push(this.drawingObject.getById(this.fieldList[column * this.size + row]));
+				tilesDrawables.push(this.drawable.getById(this.fieldList[column * this.size + row]));
+			
 				tilesDrawables.animate({
 					opacity: 1,
 					fill: this.translateColor(this.Game.currentPlayer.color)
 				}, 300, "backOut");
+			
 				this.Game.changePlayer();
-				this.updateBoardInfo();
+				this.updateScores();
+				this.updateCurrentPlayer();
+			
 				if (this.Game.pass) {
 					this.Game.changePlayer();
-					this.updateBoardInfo();
+					this.updateCurrentPlayer();
 				}
+			
 				if (this.Game.isGameFinished()) {
 					this.printWinner();
 				}
+			};
+
+			this.updateCurrentPlayer = function() {
+								this.header.attr({
+					text: this.getCurrentPlayer(this.Game.currentPlayer.color) + " Turn"
+				});
 			}
 
-			this.updateBoardInfo = function () {
+			this.updateScores = function () {
 				for (var i = this.scores.length; i > 0; --i) {
 					this.scores[i] = this.scores[i - 1];
 				}
@@ -107,26 +123,35 @@ window.onload = function () {
 				lastScores.forEach(function (score, index, scoresArray) {
 					this.scoreBoard.forEach(function (scoresDisplay, player, scoreBoardArray) {
 						var screenEdge = player === 0 ? 40 : 120;
-						var fontSize = 32 - index * 3
-						var position = 150 + index * 25
-						console.log(scoresDisplay[index]);
+						var fontSize = 32 - index * 3;
+						var position = 150 + index * 25;
 						if (scoresDisplay[index] !== undefined) {
 							scoresDisplay[index].attr({
 								text: this.scores[index][player]
 							});
 						} else {
-						scoresDisplay[index] = this.drawingObject.text(screenEdge, position).attr({
+						scoresDisplay[index] = this.drawable.text(screenEdge, position).attr({
 							font: "bold " + fontSize + "px Verdana",
-							fill: "#4F443A",
+							fill: this.blacks,
 							text: this.scores[index][player]
 						});
 					}
-					}, this)
+					}, this);
 				}, this);
-				this.header.attr({
-					text: this.getCurrentPlayer(this.Game.currentPlayer.color) + " Turn"
-				});
+			};
+
+			this.playAgain = function(obj) {
+				return function() {
+				var othello = document.getElementById("othello");
+				othello.parentNode.removeChild(othello);
+				var _body = document.getElementsByTagName('body') [0];
+				var _div = document.createElement('div');
+				_div.setAttribute("id", "othello");
+				_body.appendChild(_div);
+				obj.initialise();
+				obj.drawBoard();
 			}
+			};
 
 			this.printWinner = function () {
 				var winner;
@@ -139,50 +164,58 @@ window.onload = function () {
 						winner = MY.colorsEnum.empty;
 					}
 				});
-				this.drawingObject.text(this.gridsize / 2, this.gridsize - 40).attr({
+				this.drawable.text(this.gridsize / 2, this.gridsize - 40).attr({
 					font: "bold 40px Verdana",
-					fill: "#4F443A",
+					fill: this.blacks,
 					text: this.getCurrentPlayer(winner.color) + " Win"
 				});
 
-			}
+			};
 
 			this.translateColor = function (colorEnum) {
 				return colorEnum === MY.colorsEnum.white ? this.whites : this.blacks;
-			}
+			};
 
 			this.getCurrentPlayer = function (color) {
 				return color === MY.colorsEnum.white ? "Whites" :  color === MY.colorsEnum.black ? "Blacks" : "Draw";
-			}
+			};
 
 			this.drawBoard = function () {
-				var canvas =  document.getElementById("othello");
-				canvas.style.width = this.gridsize + "px";
-				canvas.style.margin = "0 auto";
-				this.drawingObject = Raphael("othello", this.gridsize, this.gridsize);
-				this.header = this.drawingObject.text(this.gridsize / 2 + 0.75 * this.baseOffset, 50).attr({
+				var _canvas =  document.getElementById("othello");
+				_canvas.style.width = this.gridsize + "px";
+				_canvas.style.margin = "0 auto";
+				this.drawable = Raphael("othello", this.gridsize, this.gridsize);
+				this.header = this.drawable.text(this.gridsize / 2, 50).attr({
 					font: "bold 40px Verdana",
-					fill: "#4F443A",
+					fill: this.blacks,
 					text: "Blacks Turn"
 				});
-				var playerScores = this.drawingObject.text(80, 105, "Scores").attr({
+				var playerScores = this.drawable.text(80, 105, "Scores").attr({
 					font: "bold 20px Verdana",
-					fill: "#7F8E98"
+					fill: this.boardColorDark
 				});
-				var whitePlayerScores = this.drawingObject.text(40, 130, "Whites").attr({
+				var whitePlayerScores = this.drawable.text(40, 130, "Whites").attr({
 					font: "bold 16px Verdana",
-					fill: "#4F443A"
+					fill: this.blacks
 				});
-				var blackPlayerScores = this.drawingObject.text(120, 130, "Blacks").attr({
+				var blackPlayerScores = this.drawable.text(120, 130, "Blacks").attr({
 					font: "bold 16px Verdana",
-					fill: "#4F443A"
+					fill: this.blacks
 				});
+
+				var shiftColours = function(colour) {
+					return function() {
+						this.animate({fill: colour}, 200, "backOut");
+					}
+				}
+				var newGame = this.drawable.text(80, 610, "Restart").attr({
+					font: "bold 32px Verdana",
+					fill: this.blacks
+				}).click(this.playAgain(this)).hover(shiftColours(this.boardColorDark),shiftColours(this.blacks));
 				var path = "M0 " + 92 + " L" + this.gridsize + " " + 92;
-				console.log(path);
-				var separator = this.drawingObject.path(path);
+				var separator = this.drawable.path(path);
 				var path2 = "M0 " + 635 + " L" + this.gridsize + " " + 635;
-				console.log(path2);
-				var separator2 = this.drawingObject.path(path2);
+				var separator2 = this.drawable.path(path2);
 
 				for (var i = 0; i < this.size * this.size; ++i) {
 					var column = Math.floor(i / this.size);
@@ -192,20 +225,12 @@ window.onload = function () {
 					var fieldId = this.field(xcord, ycord, row, column);
 					this.fieldList[row * this.size + column] = fieldId;
 				}
-				this.updateBoardInfo();
-			}
-		}
+				this.updateScores();
+				this.updateCurrentPlayer();
+			};
+		};
 
 	var UI = new ReversiUI();
 	UI.initialise();
 	UI.drawBoard();
-	/*var othello = document.getElementById("othello");
-	othello.parentNode.removeChild(othello);
-	var _body = document.getElementsByTagName('body') [0];
-	var _div = document.createElement('div');
-	_div.setAttribute("id", "othello");
-	_body.appendChild(_div);
-	UI.initialise();
-	UI.drawBoard();*/
-
-}
+};
